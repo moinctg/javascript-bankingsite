56f43d55f56ad7312ed515ad183c9785bb89f1af
moinuddin kamal <cpimoinuddin@gmail.com>
2021-08-19 22:34:16 +0600

    deposit adding

--- js/banking.js
+++ js/banking.js
@@ -1,7 +1,8 @@
 
 // Handle Deposit Button Event 
 
-document.getElementById('deposit-button',addEventListener('click',function(){
+document.getElementById('deposit-button').addEventListener('click',function(){
+    // console.log('depoist click');
 
     // get amount deposited 
     const deposit = document.getElementById('deposit-input');
@@ -11,20 +12,18 @@ document.getElementById('deposit-button',addEventListener('click',function(){
     // console.log(depositAmount);
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
-
-    
-
+    // depositTotal.innerText = depositAmount;
+   if(depositAmount>0){
     const priviousDepositAmountText = depositTotal.innerText;
     const priviousDepositAmount = parseFloat(priviousDepositAmountText);
     const newDepositAmount = priviousDepositAmount + depositAmount;
 
      depositTotal.innerText = newDepositAmount;
-     
+   }
 
      // clear deposit input 
 
     deposit.value='';
-}));
+});
 
 
